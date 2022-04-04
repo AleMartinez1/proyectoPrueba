f23115af29864892237df94f9c69a5a8ee153970
alejandroMartinez <alejandro.martinez@landscape.cl>
2022-04-04 19:11:25 -0300

    agregar comandos

--- cypress/integration/examples/PrimerasPruebas.js
+++ cypress/integration/examples/PrimerasPruebas.js
@@ -18,11 +18,16 @@ it('prueba primer caso - Ingreso Recepcionista', function(){
     // ingresamos a la pagina
     cy.visit("https://cloudrayweb.wolvex.cl/login")
 
+    //login con usuario Recepcinista
     cy.get(':nth-child(2) > input').type('0001')
     cy.get(':nth-child(4) > input').type('florinda.mesa@cloudray.cl')
     cy.get(':nth-child(6) > input').type('User1234567')
     cy.get('.flexboxButton').click()
- 
+   
+
+    //Loguin DDT - Cargamos datos desde el fixture
+    //cy.get(':nth-child(2) > input').type(this.datos.codigoCentro)
+    //cy.get(':nth-child(4) > input').type(this.datos.email)
 
 })
  
@@ -41,9 +46,11 @@ it('Nuevo Paciente', function(){
      cy.get('#phonePaciente').type('38157554')
      cy.get('#emailPaciente').type('prueba@prueba.com')
      cy.get('.saveButton').click()
-     cy.get('.continueButton').click()
+     //cy.get('.continueButton').click()
      //cy.get('[style="background: rgb(1, 115, 153); border: none; border-radius: 10px; font-weight: 600; font-size: 16px; color: rgb(255, 255, 255); padding: 10px 96px;"]').click()     
      //cy.get('.continueButton').click()
+
+     //cy.NuevoPaciente()
 })
 //Caso de prueba 3
  
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,19 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("NuevoPaciente", () => {
+
+    //Carga de datos para el alta
+    cy.get('.justify-content-between > :nth-child(3) > .btn').click()
+    cy.get(':nth-child(2) > .form-control').select('ARS')
+    cy.get(':nth-child(3) > .form-control').type('121212333')
+    cy.get(':nth-child(4) > .input-group > .input-group-append > .btn > .far').click()
+    cy.get('.right > .btn > .ngb-dp-navigation-chevron').click()
+    cy.get(':nth-child(6) > :nth-child(1) > .btn-light').click()
+    cy.get('.d-block').click() //Requiere AVAL
+    cy.get(':nth-child(6) > .form-control').select('SGR 11')
+    cy.get(':nth-child(7) > .form-control').select('CAPITAL MARKETS ARGENTINA-CHQ.DIF. - 5851 - DUHAU S.A. - 000004437 - MROS')
+    cy.get('.btn-primary').click()
+
+})
