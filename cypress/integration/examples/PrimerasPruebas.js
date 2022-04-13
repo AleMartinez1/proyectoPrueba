f13bf3577f7ce7d18b73b1949ea75619c51c6187
alejandroMartinez <alejandro.martinez@landscape.cl>
2022-04-13 14:06:23 -0300

    Pruebas para realizar pago de examenes

--- cypress/integration/examples/PrimerasPruebas.js
+++ cypress/integration/examples/PrimerasPruebas.js
@@ -4,9 +4,9 @@
 describe('Primer conjunto de casos de prueba',function()
 {
 
-    before(function () {
+    this.beforeEach(function () {
         //cargamos los valores del archivo loginAdministrador
-        cy.fixture('loginRecepcionista').then(function (datos) {
+        cy.fixture('NuevoPaciente').then(function (datos) {
             this.datos = datos
         })
 
@@ -19,44 +19,42 @@ it('prueba primer caso - Ingreso Recepcionista', function(){
     cy.visit("https://cloudrayweb.wolvex.cl/login")
 
     //login con usuario Recepcinista
-    cy.get(':nth-child(2) > input').type('0001')
+    cy.loginRecepcionista()
+
+    /*cy.get(':nth-child(2) > input').type('0001')
     cy.get(':nth-child(4) > input').type('florinda.mesa@cloudray.cl')
     cy.get(':nth-child(6) > input').type('User1234567')
-    cy.get('.flexboxButton').click()
+    cy.get('.flexboxButton').click()*/
    
-
-    //Loguin DDT - Cargamos datos desde el fixture
-    //cy.get(':nth-child(2) > input').type(this.datos.codigoCentro)
-    //cy.get(':nth-child(4) > input').type(this.datos.email)
-
 })
  
 //Caso de prueba 2
 it('Ficha Nuevo Paciente', function(){
  
      // ingresamos a la pagina
-     //cy.visit("https://cloudrayweb.wolvex.cl/login")
+     
      cy.get('.sidenav > :nth-child(1) > :nth-child(4)').click()
-     cy.get('#passportOrRut').type('19562178-0')
-     cy.get('.form-patientRegister > :nth-child(2) > input').type('Alejandro')
-     cy.get('#apellidoPaternoPaciente').type('Martinez')
-     cy.get('#appelidoMaternoPaciente').type('Aguero')
-     cy.get('#nacimientoPaciente').clear().type('07/12/1991')
-     cy.get('#sexPatient').select('Masculino')
-     cy.get(':nth-child(5) > :nth-child(1) > h3').should('contain.text','Teléfono')
-     cy.get('#phonePaciente').type('38157554')
-     cy.get('#emailPaciente').type('prueba@prueba.com')
+     cy.get('[style="margin-left: 26px;"] > .titleBar').should('contain.text','Ficha paciente')
+     cy.get('#passportOrRut').type(this.datos.Documento)
+     cy.get('.form-patientRegister > :nth-child(2) > input').type(this.datos.Nombre)
+     cy.get('#apellidoPaternoPaciente').type(this.datos.ApellidoPaterno)
+     cy.get('#appelidoMaternoPaciente').type(this.datos.ApellidoMaterno)
+     cy.get('#nacimientoPaciente').clear().type(this.datos.FechaNac)
+     cy.get('#sexPatient').select(this.datos.Sexo)
+     cy.get('#phonePaciente').type(this.datos.teléfono)
+     cy.get('#emailPaciente').type(this.datos.Email)
      cy.get('.saveButton').click()
+     cy.get('#errorCardPatientRegister').should('contain.text','Paciente guardado con éxito')
      cy.get('.continueButton').click()
-     cy.get('[style="background: rgb(1, 115, 153); border: none; border-radius: 10px; font-weight: 600; font-size: 16px; color: rgb(255, 255, 255); padding: 10px 96px;"]').click()
-     //cy.get('.continueButton').click()
+     //cy.get('[style="background: rgb(1, 115, 153); border: none; border-radius: 10px; font-weight: 600; font-size: 16px; color: rgb(255, 255, 255); padding: 10px 96px;"]').click()
+    
 
-     //cy.NuevoPaciente()
+     
 })
 //Caso de prueba 3
 it('Ficha nuevo examen', function(){
-    cy.get('#specialist').type('prueba')
-    cy.get('#clinic').type('clinica prb')
+    cy.get('#specialist').type('Prueba Especialista')
+    cy.get('#clinic').type('clinica prueba')
     cy.get('#email').type('asd@prueba.com')
     cy.get(':nth-child(2) > :nth-child(3) > .section_container-exams > :nth-child(1) > .input_container > .checkmark').click()
     cy.get(':nth-child(2) > :nth-child(3) > .section_container-exams > :nth-child(2) > .input_container > .checkmark').click()
@@ -64,5 +62,21 @@ it('Ficha nuevo examen', function(){
 
 })
  
+
+it('Pagar orden', function(){
+
+    cy.get('.paymentTitle').should('contain.text' , 'Registro de pago')
+    cy.get('.textType > :nth-child(4)').should('contain.text' , '38405266-5')
+    //cy.get(':nth-child(2) > .paymentInfo_container > .paymentInfo_input > .select_payment').click()
+    cy.get('#paymentType').select('Precio normal')//.should('have.value','PRECIO_NORMAL')
+    //cy.get('#clinicSelect').select()
+    //cy.get('.paymentTitle').should('contain.text' , 'Registro de pago')
+    /*cy.get(':nth-child(2) > .center_element').click()
+    cy.get('#cashInput').type('24000')
+    cy.get('.folio').type('987789')
+    cy.get('#cashInput').click()*/
+    
+
+})
  
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,18 +24,13 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-Cypress.Commands.add("NuevoPaciente", () => {
+Cypress.Commands.add("loginRecepcionista", () => {
 
-    //Carga de datos para el alta
-    cy.get('.justify-content-between > :nth-child(3) > .btn').click()
-    cy.get(':nth-child(2) > .form-control').select('ARS')
-    cy.get(':nth-child(3) > .form-control').type('121212333')
-    cy.get(':nth-child(4) > .input-group > .input-group-append > .btn > .far').click()
-    cy.get('.right > .btn > .ngb-dp-navigation-chevron').click()
-    cy.get(':nth-child(6) > :nth-child(1) > .btn-light').click()
-    cy.get('.d-block').click() //Requiere AVAL
-    cy.get(':nth-child(6) > .form-control').select('SGR 11')
-    cy.get(':nth-child(7) > .form-control').select('CAPITAL MARKETS ARGENTINA-CHQ.DIF. - 5851 - DUHAU S.A. - 000004437 - MROS')
-    cy.get('.btn-primary').click()
+    //alta recepcionista
+    cy.get(':nth-child(2) > input').type('0001')
+    cy.get(':nth-child(4) > input').type('florinda.mesa@cloudray.cl')
+    cy.get(':nth-child(6) > input').type('User1234567')
+    cy.get('.flexboxButton').click()
+    
 
 })
