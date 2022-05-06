45d9b4bf133e22e170f7b4dbb9afe92b3ff938b3
alejandroMartinez <alejandro.martinez@landscape.cl>
2022-05-06 17:51:09 -0300

    Primera prueba exitosa

--- cypress/integration/examples/PrimerasPruebas.js
+++ cypress/integration/examples/PrimerasPruebas.js
@@ -36,6 +36,7 @@ it('Ficha Nuevo Paciente', function(){
      cy.get('.sidenav > :nth-child(1) > :nth-child(4)').click()
      cy.get('[style="margin-left: 26px;"] > .titleBar').should('contain.text','Ficha paciente')
      cy.get('#passportOrRut').type(this.datos.Documento)
+     cy.get('#errorCardPatientNotFound').should('contain.text','Paciente no encontrado. Ingrese los datos del paciente en el formulario a continuación y marque la opción “Guardar” antes de continuar.')
      cy.get('.form-patientRegister > :nth-child(2) > input').type(this.datos.Nombre)
      cy.get('#apellidoPaternoPaciente').type(this.datos.ApellidoPaterno)
      cy.get('#appelidoMaternoPaciente').type(this.datos.ApellidoMaterno)
@@ -47,10 +48,10 @@ it('Ficha Nuevo Paciente', function(){
      cy.get('#errorCardPatientRegister').should('contain.text','Paciente guardado con éxito')
      cy.get('.continueButton').click()
      //cy.get('[style="background: rgb(1, 115, 153); border: none; border-radius: 10px; font-weight: 600; font-size: 16px; color: rgb(255, 255, 255); padding: 10px 96px;"]').click()
-    
-
-     
+       
 })
+
+
 //Caso de prueba 3
 it('Ficha nuevo examen', function(){
     cy.get('#specialist').type('Prueba Especialista')
@@ -66,16 +67,18 @@ it('Ficha nuevo examen', function(){
 it('Pagar orden', function(){
 
     cy.get('.paymentTitle').should('contain.text' , 'Registro de pago')
-    cy.get('.textType > :nth-child(4)').should('contain.text' , '38405266-5')
-    //cy.get(':nth-child(2) > .paymentInfo_container > .paymentInfo_input > .select_payment').click()
-    cy.get('#paymentType').select('Precio normal')//.should('have.value','PRECIO_NORMAL')
-    //cy.get('#clinicSelect').select()
-    //cy.get('.paymentTitle').should('contain.text' , 'Registro de pago')
-    /*cy.get(':nth-child(2) > .center_element').click()
-    cy.get('#cashInput').type('24000')
-    cy.get('.folio').type('987789')
-    cy.get('#cashInput').click()*/
+    cy.get('.textType > :nth-child(4)').should('contain.text' , '11504145-2')
+    cy.get('#paymentType').select('Descuento')
+    cy.get('#discountSelect').select('Sin costo')
+    cy.get('#commentsInput').type('Automation')
     
+    //cy.get(':nth-child(2) > .center_element').click()
+    //cy.get('#cashInput').type('24000')
+    //cy.get('.folio').type('98770089')
+    cy.get('.btn_orange').click()
+    //cy.get('.successModalHeader > h2').should('contai','')
+    cy.get('.successModalText').should('contain.text','El paciente está listo para ser atendido.')
+    cy.get('[style="text-align: center;"] > button').click()
 
 })
  
